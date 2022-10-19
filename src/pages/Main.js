5bd320d44afb721493c1d4b032a3836e9b08e803
Akoruu.Dev <akoruu.dev@gmail.com>
2022-10-19 00:00:11 -0300

    STYLE [Main]: Create style for Main page using styled-components from App

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,7 +1,15 @@
+import styled from "styled-components";
+
 export default function Main({ children }) {
-    return(
-        <>
-            {children}
-        </>
+    return (
+        <Container>
+            { children }
+        </Container>
     )
 }
+
+const Container = styled.main`
+    width: 100vw;
+    height: 100vh;
+    background-color: #E5E5E5;
+`;
