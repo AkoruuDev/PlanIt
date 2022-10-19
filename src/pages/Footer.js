2d1616a0612d4c0c2b3491a79d3d1cbd8dd39f3d
Akoruu.Dev <akoruu.dev@gmail.com>
2022-10-19 00:15:20 -0300

    STYLE [Footer]: Create style for Footer part page using styled-components from App

--- src/pages/Footer.js
+++ src/pages/Footer.js
@@ -1,5 +1,58 @@
+import { Link } from "react-router-dom";
+import styled from "styled-components"
+
 export default function Footer() {
-    return(
-        <></>
+    return (
+        <Container>
+            <Content>
+                <Link  to="/habitos">
+                    <MenuText>Hábitos</MenuText>
+                </Link>
+                <Link  to="/hoje">
+                    <Today>Hoje</Today>
+                </Link>
+                <Link  to="/historico">
+                    <MenuText>Histórico</MenuText>
+                </Link>
+            </Content>
+        </Container>
     )
 }
+
+const Container = styled.footer`
+    width: 100vw;
+    height: 70px;
+    background-color: #FFFFFF;
+    display: flex;
+    justify-content: center;
+    position: fixed;
+    z-index: 1;
+    bottom: 0;
+`;
+
+const Content = styled.div`
+    width: 80vw;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    position: relative;
+`;
+
+const MenuText = styled.p`
+    font-size: 23px;
+    color: #52B6FF;
+`;
+
+const Today = styled.div`
+    width: 90px;
+    height: 90px;
+    border-radius: 50%;
+    background-color: #52B6FF;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: #FFFFFF;
+    position: absolute;
+    left: calc(50% - 45px);
+    bottom: 8px;
+`;
