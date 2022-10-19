9f3760a6561e3abff816521b8bb0911388ac684f
Akoruu.Dev <akoruu.dev@gmail.com>
2022-10-19 20:13:44 -0300

    STYLE [Today]: Create and style today page from App

--- src/pages/content/Today.js
+++ src/pages/content/Today.js
@@ -1,11 +1,28 @@
 import Header from "../Header";
 import Footer from "../Footer";
+import styled from "styled-components";
+import { week, dd, mm } from "../../constants/date";
 
 export default function Today() {
     return(
         <>
             <Header />
+            <Content>
+                <Date>{`${week()}, ${dd()}/${mm()}`}</Date>
+                <>Nenhum habito concluido ainda</>
+                <>habitos</>
+            </Content>
             <Footer />
         </>
     )
 }
+
+const Content = styled.main`
+    padding: 150px 5vw;
+    height: 100vh;
+`
+
+const Date = styled.h1`
+    color: blue;
+    font-size: 36px;
+`
